8c5b33aa78e1e2cdb4e5985ec17fa525c2593e9c
CaioRhoden <caiorhoden@gmail.com>
2025-06-03 12:42:20 -0300

    k_8 experiment with IP

--- experiments/subpartition_rag_datamodels/previews/preview_50_L2_k8/batch.sh
+++ experiments/subpartition_rag_datamodels/previews/preview_50_L2_k8/batch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=preview_50_L2
-#SBATCH --output=/home/caio.rhoden/slurm/preview_50_L2_%j.out
-#SBATCH --error=/home/caio.rhoden/slurm/preview_50_L2_%j.err
+#SBATCH --job-name=preview_k8
+#SBATCH --output=/home/caio.rhoden/slurm/preview_k8_%j.out
+#SBATCH --error=/home/caio.rhoden/slurm/preview_k8_%j.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --time=23:00:00
 #SBATCH --gres=gpu:1
-#SBATCH --mem-per-gpu=22G
+#SBATCH --mem-per-gpu=40G
 #SBATCH --mail-user=c214129@dac.unicamp.br
 #SBATCH --mail-type=BEGIN,END,FAIL
 
@@ -15,10 +15,10 @@ conda activate nq
 export WANDB_MODE="offline"
 
 # CUDA_VISIBLE_DEVICES=0 python3 pipeline.py -s setup
-CUDA_VISIBLE_DEVICES=0 python3 pipeline.py -s get_rag_retrieval
-CUDA_VISIBLE_DEVICES=0 python3 pipeline.py -s get_rag_generations
-CUDA_VISIBLE_DEVICES=0 python3 pipeline.py -s create_datamodels_datasets
-CUDA_VISIBLE_DEVICES=0 python3 pipeline.py -s run_pre_collections
+# CUDA_VISIBLE_DEVICES=0 python3 pipeline.py -s get_rag_retrieval
+# CUDA_VISIBLE_DEVICES=0 python3 pipeline.py -s get_rag_generations
+# CUDA_VISIBLE_DEVICES=0 python3 pipeline.py -s create_datamodels_datasets
+# CUDA_VISIBLE_DEVICES=0 python3 pipeline.py -s run_pre_collections
 # CUDA_VISIBLE_DEVICES=0 python3 pipeline.py -s run_collections
 # CUDA_VISIBLE_DEVICES=0 python3 pipeline.py -s train_datamodels
 # CUDA_VISIBLE_DEVICES=0 python3 pipeline.py -s evaluate_datamodels
