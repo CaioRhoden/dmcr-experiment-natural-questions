778479f4d7b8d4a76f9ba64d3a49c2b830dbfe8a
CaioRhoden <caiorhoden@gmail.com>
2025-06-06 13:04:02 -0300

    get datamodels subpartitions experiments retrieval results as jsons

--- experiments/subpartition_rag_datamodels/previews/preview_50_IP/run.sh
+++ experiments/subpartition_rag_datamodels/previews/preview_50_IP/run.sh
@@ -1,5 +1,3 @@
 
-
-CUDA_VISIBLE_DEVICES=1 python3 pipeline.py -s setup
-CUDA_VISIBLE_DEVICES=1 python3 pipeline.py -s get_rag_retrieval
-CUDA_VISIBLE_DEVICES=1 python3 pipeline.py -s get_generations
+CUDA_VISIBLE_DEVICES=1 python3 pipeline.py -s train_datamodels
+CUDA_VISIBLE_DEVICES=1 python3 pipeline.py -s evaluate_datamodels
