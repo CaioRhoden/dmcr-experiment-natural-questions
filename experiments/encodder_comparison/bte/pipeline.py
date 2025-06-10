c080c8cde5ea784df6def8b95e305d158f2a587a
CaioRhoden <caiorhoden@gmail.com>
2025-06-10 20:44:13 -0300

    change pipelines path

--- experiments/encodder_comparison/bte/pipeline.py
+++ experiments/encodder_comparison/bte/pipeline.py
@@ -1,5 +1,5 @@
 import argparse
-from utils.RAGBasedExperimentPipelineWIthConfig import RAGBasedExperimentPipelineWihConfig
+from utils.pipelines.RAGBasedExperimentPipelineWIthConfig import RAGBasedExperimentPipelineWithConfig
 
 if __name__ == "__main__":
 
@@ -9,6 +9,6 @@ if __name__ == "__main__":
 
     step = args.step
 
-    pipeline =RAGBasedExperimentPipelineWihConfig(config_path="config.yaml")
+    pipeline =RAGBasedExperimentPipelineWithConfig(config_path="config.yaml")
     pipeline.set_random_seed()
     pipeline.invoke_pipeline_stpe(step)
