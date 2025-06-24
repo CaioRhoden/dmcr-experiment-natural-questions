5bc68e024e6da11648c9cffdd3033b3aa2b47d9a
CaioRhoden <caiorhoden@gmail.com>
2025-06-24 12:36:20 -0300

    naive rag test results

--- experiments/comparison_test_dataset/run.sh
+++ experiments/comparison_test_dataset/run.sh
@@ -7,3 +7,14 @@ python run_test.py --run_type baseline --seed_idx 1
 python run_test.py --run_type baseline --seed_idx 2
 python run_test.py --run_type baseline --seed_idx 3
 python run_test.py --run_type baseline --seed_idx 4
+
+
+### NAIVE RAG GENERATIONS
+python run_test.py --run_type rag --seed_idx 0
+python run_test.py --run_type rag --seed_idx 1
+python run_test.py --run_type rag --seed_idx 2
+python run_test.py --run_type rag --seed_idx 3
+python run_test.py --run_type rag --seed_idx 4
+
+
+### DATAMODELS GENERATIONS
