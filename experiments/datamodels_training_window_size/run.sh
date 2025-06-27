68622a52a62b7ac4b855196d7cb83653a444c47d
CaioRhoden <caiorhoden@gmail.com>
2025-06-27 13:33:39 -0300

    partial training window size experiment

---
+++ experiments/datamodels_training_window_size/run.sh
@@ -0,0 +1,30 @@
+### GENERATE SEEDS
+python run_test.py --run_type setup
+
+### DATAMODELS RUN ON PRE-COLLECTIONS
+python run_test.py --run_type datamodels_pre_collections --seed_idx 0
+python run_test.py --run_type datamodels_pre_collections --seed_idx 1
+python run_test.py --run_type datamodels_pre_collections --seed_idx 2
+python run_test.py --run_type datamodels_pre_collections --seed_idx 3
+python run_test.py --run_type datamodels_pre_collections --seed_idx 4
+
+### DATAMODELS RUN ON COLLECTIONS
+python run_test.py --run_type datamodels_collections --seed_idx 0
+python run_test.py --run_type datamodels_collections --seed_idx 1
+python run_test.py --run_type datamodels_collections --seed_idx 2
+python run_test.py --run_type datamodels_collections --seed_idx 3
+python run_test.py --run_type datamodels_collections --seed_idx 4
+
+### DATAMODELS training
+python run_test.py --run_type datamodels_training --seed_idx 0
+python run_test.py --run_type datamodels_training --seed_idx 1
+python run_test.py --run_type datamodels_training --seed_idx 2
+python run_test.py --run_type datamodels_training --seed_idx 3
+python run_test.py --run_type datamodels_training --seed_idx 4
+
+### DATAMODELS GENERATIONS
+python run_test.py --run_type datamodels_generations --seed_idx 0
+python run_test.py --run_type datamodels_generations --seed_idx 1
+python run_test.py --run_type datamodels_generations --seed_idx 2
+python run_test.py --run_type datamodels_generations --seed_idx 3
+python run_test.py --run_type datamodels_generations --seed_idx 4
