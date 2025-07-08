c471efde668e049fc354ad87d7277184b6c0ee96
CaioRhoden <caiorhoden@gmail.com>
2025-07-08 20:23:17 -0300

    create utils function to random generate a dataframe of specfic size

--- experiments/subpartition_rag_datamodels/previews/generate_random_dataset.py
+++ experiments/subpartition_rag_datamodels/previews/generate_random_dataset.py
@@ -1,27 +1,6 @@
 import polars as pl
-import random
-
-seed = 42
-# NumPy
-pl.set_random_seed(seed)
 import argparse
-
-def get_random_test_dataset(n_samples: int, save_path: str) -> None:
-
-    """
-    Sample a random subset of the NQ Open test dataset.
-
-    Args:
-        n_samples: The number of examples to sample.
-        save_path: The path to save the sampled dataset.
-
-    Returns:
-        None
-    """
-    NQ_PATH = "../../data/nq_open_gold/processed/test.feather"
-
-    nq_test = pl.read_ipc(NQ_PATH).sample(n=n_samples, seed=seed)
-    nq_test.write_ipc(save_path)
+from utils.get_random_nq_dataset import get_random_nq_dataset
 
 
 if __name__ == "__main__":
@@ -34,4 +13,4 @@ if __name__ == "__main__":
     n_samples = args.n_samples
     save_path = args.save_path
 
-    get_random_test_dataset(n_samples=n_samples, save_path=save_path)
+    get_random_nq_dataset(root_path="../..", n_samples=n_samples, save_path=save_path)
