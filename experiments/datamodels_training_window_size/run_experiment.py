0d2e0f08b1f7088a98270667d574dfd1a454b06e
CaioRhoden <caiorhoden@gmail.com>
2025-07-13 21:06:56 -0300

    first and second seed 2000 size collection generations and models

--- experiments/datamodels_training_window_size/run_experiment.py
+++ experiments/datamodels_training_window_size/run_experiment.py
@@ -187,7 +187,6 @@ if __name__ == "__main__":
 
     seeds = np.load("random_seeds.npy")
     seed = int(seeds[args.seed_idx])
-    args.model_run_id = f"{args.run_type}_{seed}"
     set_random_seed(seed)
     print(f"Using seed: {seed}")
 
