22e01604311ceecd5dd32a27230832c4d316b1c0
CaioRhoden <caiorhoden@gmail.com>
2025-07-21 16:29:22 +0000

    bugfix weights to json

--- utils/weights_to_json.py
+++ utils/weights_to_json.py
@@ -2,7 +2,7 @@ import torch
 import json
 import numpy as np
 
-def weights_to_dict(weights: torch.Tensor, subset_documents: dict[str, list[int]]) -> list[dict[str, list[int]]]:
+def weights_to_dict(weights: torch.Tensor, subset_documents: dict[str, list[int]], num_models=50) -> list[dict[str, list[int]]]:
 
     """
     Convert a tensor of weights to a dictionary of the top k values for each row.
@@ -24,11 +24,11 @@ def weights_to_dict(weights: torch.Tensor, subset_documents: dict[str, list[int]
     new_dict1 = {}
     new_dict2 = {}
 
-    for i in range(50):
+    for i in range(num_models):
         key = str(i)
         orig_list = subset_documents[key]
         row = weights[i]
-        sorted_indices = np.argsort(row)[::-1]
+        sorted_indices = np.argsort(row)
 
         
         # Create new_dict1: reorder the original list based on descending order of the tensor row
@@ -36,7 +36,7 @@ def weights_to_dict(weights: torch.Tensor, subset_documents: dict[str, list[int]
         new_dict1[key] = new_list
         
         # Create new_dict2: reverse the tensor row and convert to list
-        reversed_row = row[::-1]
+        reversed_row = row[sorted_indices]
         new_dict2[key] = reversed_row.tolist()
     
     
@@ -48,6 +48,7 @@ def load_weights_to_json(
         subset_documents_path: str,
         saving_path: str,
         saving_id: str,
+        num_models=50
     ):
 
 
@@ -70,7 +71,7 @@ def load_weights_to_json(
     
     weights = torch.load(weights_path, weights_only=True)
     subset_documents = json.load(open(subset_documents_path, "r"))
-    result = weights_to_dict(weights, subset_documents)
+    result = weights_to_dict(weights, subset_documents, num_models=num_models)
 
     print("Saving weights to json in: ", saving_path)
     json.dump(result[0], open(f"{saving_path}/{saving_id}_indexes.json", "w"))
