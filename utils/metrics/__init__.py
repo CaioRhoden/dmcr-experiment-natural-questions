0d5c6bc669ad24d2e58e8fb92b016d6503c0932e
CaioRhoden <caiorhoden@gmail.com>
2025-07-28 20:05:42 -0300

    fixing normalization bug

---
+++ utils/metrics/__init__.py
@@ -0,0 +1,4 @@
+from .calculate_metric import calculate_agg_metric
+from .calculate_perplexity import calculate_perplexity, calculate_batch_perplexity
+
+__all__ = ["calculate_agg_metric", "calculate_perplexity", "calculate_batch_perplexity"]
