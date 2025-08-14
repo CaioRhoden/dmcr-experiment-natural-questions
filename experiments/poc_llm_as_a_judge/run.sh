309c8433e7c0a94cadc4677630decf17536abcfc
CaioRhoden <caiorhoden@gmail.com>
2025-08-14 20:06:34 -0300

    llm as judge pre_collections

--- experiments/poc_llm_as_a_judge/run.sh
+++ experiments/poc_llm_as_a_judge/run.sh
@@ -1,52 +1,33 @@
-### GENERATE SEEDS
-python run_experiment.py --run_type setup
+#!/bin/bash
+#SBATCH --job-name=llm_as_judge_generation_7270_
+#SBATCH --output=/home/caio.rhoden/slurm/llm_as_judge_generation_7270_%j.out
+#SBATCH --error=/home/caio.rhoden/slurm/llm_as_judge_generation_7270_%j.err
+#SBATCH --gres=gpu:1
+#SBATCH --mem-per-gpu=15G
+#SBATCH --mail-user="c214129@dac.unicamp.br"
+#SBATCH --mail-type=BEGIN,END,FAIL
 
-
-## BASELINE GENERARTIONS
-python run_experiment.py --run_type baseline --seed_idx 0
-python run_experiment.py --run_type baseline --seed_idx 1
-python run_experiment.py --run_type baseline --seed_idx 2
-python run_experiment.py --run_type baseline --seed_idx 3
-python run_experiment.py --run_type baseline --seed_idx 4
-
-## RAG Generatiosn
-python run_experiment.py --run_type rag --seed_idx 0
-python run_experiment.py --run_type rag --seed_idx 1
-python run_experiment.py --run_type rag --seed_idx 2
-python run_experiment.py --run_type rag --seed_idx 3
-python run_experiment.py --run_type rag --seed_idx 4
-
-## Datamodels Setup
-python run_experiment.py --run_type datamodels_setup --seed_idx 0
-python run_experiment.py --run_type datamodels_setup --seed_idx 1
-python run_experiment.py --run_type datamodels_setup --seed_idx 2
-python run_experiment.py --run_type datamodels_setup --seed_idx 3
-python run_experiment.py --run_type datamodels_setup --seed_idx 4
-
-### DATAMODELS RUN ON PRE-COLLECTIONS
-python run_experiment.py --run_type datamodels_pre_collections --seed_idx 0
-python run_experiment.py --run_type datamodels_pre_collections --seed_idx 1
-python run_experiment.py --run_type datamodels_pre_collections --seed_idx 2
-python run_experiment.py --run_type datamodels_pre_collections --seed_idx 3
-python run_experiment.py --run_type datamodels_pre_collections --seed_idx 4
+source ~/miniconda3/bin/activate
+conda activate nq
+export WANDB_MODE="offline"
 
 ### DATAMODELS RUN ON COLLECTIONS
-python run_experiment.py --run_type datamodels_collections --seed_idx 0
-python run_experiment.py --run_type datamodels_collections --seed_idx 1
-python run_experiment.py --run_type datamodels_collections --seed_idx 2
-python run_experiment.py --run_type datamodels_collections --seed_idx 3
-python run_experiment.py --run_type datamodels_collections --seed_idx 4
+# python run_experiment.py --run_type datamodels_collections --seed_idx 0 --train_checkpoint 1000 --test_checkpoint 1000 --train_start_idx 10000 --train_end_idx 20000
+# python run_experiment.py --run_type datamodels_collections --seed_idx 1
+# python run_experiment.py --run_type datamodels_collections --seed_idx 2
+# python run_experiment.py --run_type datamodels_collections --seed_idx 3
+# python run_experiment.py --run_type datamodels_collections --seed_idx 4
 
-### DATAMODELS training
-python run_experiment.py --run_type datamodels_training --seed_idx 0
-python run_experiment.py --run_type datamodels_training --seed_idx 1
-python run_experiment.py --run_type datamodels_training --seed_idx 2
-python run_experiment.py --run_type datamodels_training --seed_idx 3
-python run_experiment.py --run_type datamodels_training --seed_idx 4
+# ### DATAMODELS training
+# python run_experiment.py --run_type datamodels_training --seed_idx 0
+# python run_experiment.py --run_type datamodels_training --seed_idx 1
+# python run_experiment.py --run_type datamodels_training --seed_idx 2
+# python run_experiment.py --run_type datamodels_training --seed_idx 3
+# python run_experiment.py --run_type datamodels_training --seed_idx 4
 
-### DATAMODELS GENERATIONS
-python run_experiment.py --run_type datamodels_generations --seed_idx 0
-python run_experiment.py --run_type datamodels_generations --seed_idx 1
-python run_experiment.py --run_type datamodels_generations --seed_idx 2
-python run_experiment.py --run_type datamodels_generations --seed_idx 3
-python run_experiment.py --run_type datamodels_generations --seed_idx 4
+# ### DATAMODELS GENERATIONS
+python run_experiment.py --run_type datamodels_generations --seed_idx 0 --datamodels_generation_name judge_generation --model_run_id judge_llm
+# python run_experiment.py --run_type datamodels_generations --seed_idx 1
+# python run_experiment.py --run_type datamodels_generations --seed_idx 2
+# python run_experiment.py --run_type datamodels_generations --seed_idx 3
+# python run_experiment.py --run_type datamodels_generations --seed_idx 4
