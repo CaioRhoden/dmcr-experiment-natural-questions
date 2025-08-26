38261fa8f8bf6c2ad8d07410605e9f97ce15f260
CaioRhoden <caiorhoden@gmail.com>
2025-08-26 17:05:36 -0300

    updating utils

--- utils/pipelines/__init__.py
+++ utils/pipelines/__init__.py
@@ -1,5 +1,4 @@
 from .RAGBasedExperimentPipeline import RAGBasedExperimentPipeline
-from .RAGBasedExperimentPipelineWIthConfig import RAGBasedExperimentPipelineWithConfig
 from .LassoExperimentPipeline import LassoExperimentPipeline
 from .BaselinePipelineWithConfig import BaselinePipelineWithConfig
 from .BaselinePipeline import BaselinePipeline
