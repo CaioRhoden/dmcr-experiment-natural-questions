5a1cfb8f96d15457eae95c60c1c39826c91bce5a
CaioRhoden <caiorhoden@gmail.com>
2025-08-26 15:26:40 -0300

    clean experiments page

--- experiments/subpartition_rag_datamodels/previews/preview_100/run.sh
+++
@@ -1,5 +0,0 @@
-
-
-CUDA_VISIBLE_DEVICES=0 python3 pipeline.py -s setup
-CUDA_VISIBLE_DEVICES=0 python3 pipeline.py -s get_rag_retrieval
-CUDA_VISIBLE_DEVICES=0 python3 pipeline.py -s get_generations
