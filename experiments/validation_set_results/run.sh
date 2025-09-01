6be5e9a38c39ade3d01020a18d470e27044ee044
CaioRhoden <caiorhoden@gmail.com>
2025-09-01 19:25:18 +0000

    zero shot results llama 3.2 dev and test sets generations

--- experiments/validation_set_results/run.sh
+++ experiments/validation_set_results/run.sh
@@ -1,2 +1,3 @@
-## Run zero shot lllama
-python3 run_zero_shot.py --model Llama-3.2-3B-Instruct --
+## Run zero shot lllama 3.2 3B
+python3 run_zero_shot.py --model Llama-3.2-3B-Instruct --batch_size 8 --language_model_path models/llms/Llama-3.2-3B-Instruct
+
