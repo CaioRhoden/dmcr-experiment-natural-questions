9ff226eb72ab1f6fa2a8d5b81413a507c5467b4c
CaioRhoden <caiorhoden@gmail.com>
2025-10-14 09:10:33 -0300

    instructiojn bias for recall analysis in RAG generation

---
+++ experiments/prompt_analysis_recall/run.sh
@@ -0,0 +1,13 @@
+source ~/miniconda3/bin/activate
+conda activate nq
+export WANDB_MODE="offline"
+
+
+python3 generate_random_samples.py --n_samples 1000 --seed 42 --partition dev
+
+
+## Run all RAG
+# sh scripts/run_rag.sh
+
+## DATAMODELS
+### 
