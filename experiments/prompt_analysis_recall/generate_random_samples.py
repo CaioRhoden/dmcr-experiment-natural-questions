618e01d6efb307b6befa62c7a28ece1c3e032b3b
CaioRhoden <caiorhoden@gmail.com>
2025-10-14 09:10:33 -0300

    instructiojn bias for recall analysis in RAG generation

---
+++ experiments/prompt_analysis_recall/generate_random_samples.py
@@ -0,0 +1,27 @@
+import argparse
+from utils.get_random_nq_dataset import get_random_nq_dataset
+from utils.set_random_seed import set_random_seed
+import random
+
+set_random_seed(42)
+
+if __name__ == "__main__":
+
+    seed = random.randint(0,1000)
+    args = argparse.ArgumentParser()
+    args.add_argument("--n_samples", type=int, default=500)
+    args.add_argument("--seed", type=int, default=seed)
+    args.add_argument("--partition", type=str, default="dev")
+
+    args = args.parse_args()
+    print(f"Args: {args}")
+
+    get_random_nq_dataset(
+        root_path="../..",
+        n_samples=args.n_samples,
+        save_path=f"questions_{args.n_samples}_{args.seed}_{args.partition}.feather",
+        partition=args.partition,
+        seed=args.seed
+    )
+
+    
