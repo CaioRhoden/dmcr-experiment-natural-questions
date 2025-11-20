54cb8f7772e4bc3b13d3a41125d0fd498aaccf6d
CaioRhoden <caiorhoden@gmail.com>
2025-11-20 00:50:04 -0300

    hard reset vector database problem

--- experiments/debugging_rag_recall_vllm_vs_hfpipeline/validating_datamodels/scripts/rouge_collections.sh
+++ experiments/debugging_rag_recall_vllm_vs_hfpipeline/validating_datamodels/scripts/rouge_collections.sh
@@ -4,15 +4,15 @@
 #SBATCH --error=/home/caio.rhoden/slurm/%A_%a_generating_rag_debbuging_validation.err
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem-per-gpu=30G
+#SBATCH --mem-per-gpu=22G
 #SBATCH --time=48:00:00
 #SBATCH --mail-user="c214129@dac.unicamp.br"
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --partition=a5000
-#SBATCH --array=0-3
+#SBATCH --array=0
 
-START_IDX=$((60000000 + $SLURM_ARRAY_TASK_ID * 1000000))
-END_IDX=$(($START_IDX + 1000000))
+START_IDX=620000
+END_IDX=700000
 
 
 
@@ -24,9 +24,9 @@ export WANDB_MODE="offline"
 echo "-----------------------------------------------"
 echo "RUNNING PRE_COLLECTIONS TRAIN "
 python run_datamodels.py \
-    --seed $1 \
+    --seed 1 \
     --instruction_idx 0 \
-    --run_type pre_collections \
+    --run_type collections \
     --start_idx $START_IDX \
     --end_idx $END_IDX \
     --checkpoint 20000 \
