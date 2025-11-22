e549e2d36a2ec7ec1d22b79400e2a51c1a98e308
CaioRhoden <caiorhoden@gmail.com>
2025-11-22 16:50:05 -0300

    git lfs reset

--- experiments/debugging_rag_recall_vllm_vs_hfpipeline/validating_datamodels/scripts/pre_collections.sh
+++ experiments/debugging_rag_recall_vllm_vs_hfpipeline/validating_datamodels/scripts/pre_collections.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem-per-gpu=43G
 #SBATCH --time=48:00:00
 #SBATCH --mail-user="c214129@dac.unicamp.br"
-#SBATCH --array=2-4
+#SBATCH --array=1-4
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 source ~/miniconda3/bin/activate
@@ -16,42 +16,41 @@ export VLLM_WORKER_MULTIPROC_METHOD=spawn
 export C_INCLUDE_PATH=$CONDA_PREFIX/include
 export CPLUS_INCLUDE_PATH=$CONDA_PREFIX/include
 
-SEEDS=(1 4 54 61 73)
+SEEDS=(4 54 61 73)
 INSTUCTIONS=(0)
+S=${SEEDS[$SLURM_ARRAY_TASK_ID]}
 
-for S in "${SEEDS[@]}"; do
-    for INSTRUCTION_IDX in "${INSTUCTIONS[@]}"; do
-        
-        echo "Running setup for seed $S and instruction index $INSTRUCTION_IDX"
-        echo "-----------------------------------------------"
-        echo "RUNNING SETUP"
-        python run_datamodels.py \
-            --seed $S \
-            --instruction_idx $INSTRUCTION_IDX \
-            --run_type setup
+for INSTRUCTION_IDX in "${INSTUCTIONS[@]}"; do
+    
+    echo "Running setup for seed $S and instruction index $INSTRUCTION_IDX"
+    echo "-----------------------------------------------"
+    echo "RUNNING SETUP"
+    python run_datamodels.py \
+        --seed $S \
+        --instruction_idx $INSTRUCTION_IDX \
+        --run_type setup
 
-        echo "-----------------------------------------------"
-        echo "RUNNING PRE_COLLECTIONS TRAIN "
-        python run_datamodels.py \
-            --seed $S \
-            --instruction_idx $INSTRUCTION_IDX \
-            --run_type pre_collections \
-            --start_idx 0 \
-            --end_idx 2000 \
-            --checkpoint 200 \
-            --mode train
+    echo "-----------------------------------------------"
+    echo "RUNNING PRE_COLLECTIONS TRAIN "
+    python run_datamodels.py \
+        --seed $S \
+        --instruction_idx $INSTRUCTION_IDX \
+        --run_type pre_collections \
+        --start_idx 0 \
+        --end_idx 2000 \
+        --checkpoint 200 \
+        --mode train
 
-        echo "RUNNING PRE_COLLECTIONS TEST"
-        python run_datamodels.py \
-            --seed $S \
-            --instruction_idx $INSTRUCTION_IDX \
-            --run_type pre_collections \
-            --start_idx 0 \
-            --end_idx 200 \
-            --checkpoint 200 \
-            --mode test
+    echo "RUNNING PRE_COLLECTIONS TEST"
+    python run_datamodels.py \
+        --seed $S \
+        --instruction_idx $INSTRUCTION_IDX \
+        --run_type pre_collections \
+        --start_idx 0 \
+        --end_idx 200 \
+        --checkpoint 200 \
+        --mode test
 
-      
-    done
-        
+    
 done
+        
