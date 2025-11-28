158d18f9854326fc2aef959292f2997ac76264ff
CaioRhoden <caiorhoden@gmail.com>
2025-11-28 00:21:22 -0300

    juudge collections train insstruction 0-1

--- experiments/debugging_rag_recall_vllm_vs_hfpipeline/validating_datamodels/scripts/judge_collections.sh
+++ experiments/debugging_rag_recall_vllm_vs_hfpipeline/validating_datamodels/scripts/judge_collections.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=pc_rag_debbuging_validation
-#SBATCH --output=/home/caio.rhoden/slurm/%A_%a_pc_rag_debbuging_validation.out
-#SBATCH --error=/home/caio.rhoden/slurm/%A_%a_pc_rag_debbuging_validation.err
+#SBATCH --output=/home/users/caio.rhoden/slurm/%A_%a_pc_rag_debbuging_validation.out
+#SBATCH --error=/home/users/caio.rhoden/slurm/%A_%a_pc_rag_debbuging_validation.err
 #SBATCH --gres=gpu:1
-#SBATCH --mem-per-gpu=44G
+#SBATCH --mem-per-gpu=138G
 #SBATCH --time=48:00:00
 #SBATCH --mail-user="c214129@dac.unicamp.br"
-#SBATCH --array=3-4
+#SBATCH --array=0-4
+#SBATCH --nodelist=gpu02,gpu01
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 source ~/miniconda3/bin/activate
@@ -21,6 +22,11 @@ INSTUCTIONS=(0 1)
 S=${SEEDS[$SLURM_ARRAY_TASK_ID]}
 
 for INSTRUCTION_IDX in "${INSTUCTIONS[@]}"; do
+        echo "RUNNING SETUP"
+        python run_datamodels.py \
+            --seed $S \
+            --instruction_idx $INSTRUCTION_IDX \
+            --run_type setup
     
         echo "RUNNING COLLECTIONS TRAIN"
         python run_datamodels.py \
