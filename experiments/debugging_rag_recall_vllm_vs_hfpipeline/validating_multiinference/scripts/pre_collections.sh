a62756f433e3a06c65bf68496d65e647d1fa27d4
CaioRhoden <caiorhoden@gmail.com>
2025-12-12 13:53:23 -0300

    parrtial pre collectiongs multi sentences inference

--- experiments/debugging_rag_recall_vllm_vs_hfpipeline/validating_multiinference/scripts/pre_collections.sh
+++ experiments/debugging_rag_recall_vllm_vs_hfpipeline/validating_multiinference/scripts/pre_collections.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=pc_rag_debbuging_validation
-#SBATCH --output=/home/caio.rhoden/slurm/%A_%a_pc_rag_debbuging_validation.out
-#SBATCH --error=/home/caio.rhoden/slurm/%A_%a_pc_rag_debbuging_validation.err
+#SBATCH --output=/home/users/caio.rhoden/slurm/%A_%a_pc_rag_debbuging_validation.out
+#SBATCH --error=/home/users/caio.rhoden/slurm/%A_%a_pc_rag_debbuging_validation.err
 #SBATCH --gres=gpu:1
-#SBATCH --mem-per-gpu=44G
+#SBATCH --mem-per-gpu=139G
 #SBATCH --time=48:00:00
 #SBATCH --mail-user="c214129@dac.unicamp.br"
 #SBATCH --array=0-4
 #SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --exclude=gpu03
 
 source ~/miniconda3/bin/activate
 conda activate nq
@@ -21,35 +22,39 @@ NUM_SENTENCES=(3 5)
 S=${SEEDS[$SLURM_ARRAY_TASK_ID]}
 
 for NUM_SENTENCES_IDX in "${NUM_SENTENCES[@]}"; do
+
+    BATCH_SIZE=$((NUM_SENTENCES_IDX * 500))
     
-    echo "Running setup for seed $S and instruction index $INSTRUCTION_IDX"
+    # echo "Running setup for seed $S and instruction index $INSTRUCTION_IDX"
+    # echo "-----------------------------------------------"
+    # echo "RUNNING SETUP"
+    # python run_datamodels.py \
+    #     --seed $S \
+    #     --num_sentences $NUM_SENTENCES_IDX \
+    #     --run_type setup
+
     echo "-----------------------------------------------"
-    echo "RUNNING SETUP"
+    echo "RUNNING PRE_COLLECTIONS TRAIN "
     python run_datamodels.py \
         --seed $S \
         --num_sentences $NUM_SENTENCES_IDX \
-        --run_type setup
+        --run_type pre_collections \
+        --start_idx 0 \
+        --end_idx 2000 \
+        --checkpoint 200 \
+        --batch_size $BATCH_SIZE \
+        --mode train
 
-    # echo "-----------------------------------------------"
-    # echo "RUNNING PRE_COLLECTIONS TRAIN "
-    # python run_datamodels.py \
-    #     --seed $S \
-    #     --instruction_idx $INSTRUCTION_IDX \
-    #     --run_type pre_collections \
-    #     --start_idx 0 \
-    #     --end_idx 2000 \
-    #     --checkpoint 200 \
-    #     --mode train
-
-    # echo "RUNNING PRE_COLLECTIONS TEST"
-    # python run_datamodels.py \
-    #     --seed $S \
-    #     --instruction_idx $INSTRUCTION_IDX \
-    #     --run_type pre_collections \
-    #     --start_idx 0 \
-    #     --end_idx 200 \
-    #     --checkpoint 200 \
-    #     --mode test
+    echo "RUNNING PRE_COLLECTIONS TEST"
+    python run_datamodels.py \
+        --seed $S \
+        --num_sentences $NUM_SENTENCES_IDX \
+        --run_type pre_collections \
+        --start_idx 0 \
+        --end_idx 200 \
+        --checkpoint 200 \
+        --batch_size $BATCH_SIZE \
+        --mode test
 
     
 done
