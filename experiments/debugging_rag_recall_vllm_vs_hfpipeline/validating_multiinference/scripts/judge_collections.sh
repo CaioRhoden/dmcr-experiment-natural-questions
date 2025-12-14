9b317d4b950b99712d4e52ee38289a26d82d8405
CaioRhoden <caiorhoden@gmail.com>
2025-12-13 23:49:09 -0300

    pre collections multiple sentences

--- experiments/debugging_rag_recall_vllm_vs_hfpipeline/validating_multiinference/scripts/judge_collections.sh
+++ experiments/debugging_rag_recall_vllm_vs_hfpipeline/validating_multiinference/scripts/judge_collections.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=pc_rag_debbuging_validation
-#SBATCH --output=/home/caio.rhoden/slurm/%A_%a_pc_rag_debbuging_validation.out
-#SBATCH --error=/home/caio.rhoden/slurm/%A_%a_pc_rag_debbuging_validation.err
+#SBATCH --job-name=judge_collectiong_multi_sentences
+#SBATCH --output=/home/caio.rhoden/slurm/%A_%a_judge_collectiong_multi_sentences.out
+#SBATCH --error=/home/caio.rhoden/slurm/%A_%a_judge_collectiong_multi_sentences.err
 #SBATCH --gres=gpu:1
-#SBATCH --mem-per-gpu=45G
+#SBATCH --mem-per-gpu=139G
 #SBATCH --time=48:00:00
 #SBATCH --mail-user="c214129@dac.unicamp.br"
-#SBATCH --array=0
+#SBATCH --array=0-4
 #SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --exclude=gpu03
+
 
 source ~/miniconda3/bin/activate
 conda activate nq
@@ -16,26 +18,27 @@ export VLLM_WORKER_MULTIPROC_METHOD=spawn
 export C_INCLUDE_PATH=$CONDA_PREFIX/include
 export CPLUS_INCLUDE_PATH=$CONDA_PREFIX/include
 
-SEEDS=(73)
-INSTUCTIONS=(1)
+SEEDS=(1 4 54 61 73)
+NUM_SENTENCES=(3 5)
 S=${SEEDS[$SLURM_ARRAY_TASK_ID]}
 
-for INSTRUCTION_IDX in "${INSTUCTIONS[@]}"; do
+for NUM_SENTENCES_IDX in "${NUM_SENTENCES[@]}"; do
         echo "RUNNING SETUP"
         python run_datamodels.py \
             --seed $S \
-            --instruction_idx $INSTRUCTION_IDX \
+            --num_sentences $NUM_SENTENCES_IDX \
             --run_type setup
     
         echo "RUNNING COLLECTIONS TRAIN"
         python run_datamodels.py \
             --seed $S \
-            --instruction_idx $INSTRUCTION_IDX \
+            --num_sentences $NUM_SENTENCES_IDX \
             --run_type collections \
-            --start_idx 720000 \
+            --start_idx 0 \
             --end_idx 1000000 \
             --checkpoint 20000 \
             --num_subprocesses 1 \
+            --batch_size 1000000 \
             --evaluator Judge \
             --mode train
 
