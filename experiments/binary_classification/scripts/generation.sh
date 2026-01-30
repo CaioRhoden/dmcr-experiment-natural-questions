31b62b09bb4671bc73037a2c7c485dd9b945096f
CaioRhoden <caiorhoden@gmail.com>
2026-01-30 15:02:24 -0300

    binary generations on holdout

--- experiments/binary_classification/scripts/generation.sh
+++ experiments/binary_classification/scripts/generation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=generation_model_analysis
-#SBATCH --output=/home/caio.rhoden/slurm/%A_%a_generation_model_analysis.out
-#SBATCH --error=/home/caio.rhoden/slurm/%A_%a_generation_model_analysis.err
+#SBATCH --output=/home/users/caio.rhoden/slurm/%A_%a_generation_model_analysis.out
+#SBATCH --error=/home/users/caio.rhoden/slurm/%A_%a_generation_model_analysis.err
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-gpu=44G
@@ -9,10 +9,13 @@
 #SBATCH --mail-user="c214129@dac.unicamp.br"
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --array=0-4
+#SBATCH --nodelist=gpu03
 
 source ~/miniconda3/bin/activate
 conda activate nq
-export WANDB_MODE="offline"
+# export WANDB_MODE="offline"
+export NCCL_P2P_DISABLE=1
+export NCCL_IB_DISABLE=1
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
 export C_INCLUDE_PATH=$CONDA_PREFIX/include
 export CPLUS_INCLUDE_PATH=$CONDA_PREFIX/include
@@ -39,8 +42,20 @@ S=${SEEDS[$S_ID]}
 #         --batch_size 500 \
 #         --model_tag lr_balanced_groundtruth
 
+# python run_datamodels.py \
+#         --seed $S \
+#         --run_type generation \
+#         --batch_size 500 \
+#         --model_tag lr_balanced_judge
+
+# python run_datamodels.py \
+#         --seed $S \
+#         --run_type generation \
+#         --batch_size 500 \
+#         --model_tag holdout_groundtruth
+
 python run_datamodels.py \
         --seed $S \
         --run_type generation \
         --batch_size 500 \
-        --model_tag lr_balanced_judge
+        --model_tag holdout_judge
