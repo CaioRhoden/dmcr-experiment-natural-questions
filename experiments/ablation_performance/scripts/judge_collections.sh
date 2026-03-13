56029904d1b09eaf8cec6964929adf8768eda512
CaioRhoden <caiorhoden@gmail.com>
2026-03-13 01:16:08 -0300

    ablation voting binary judges experimet 1 init

--- experiments/ablation_performance/scripts/judge_collections.sh
+++ experiments/ablation_performance/scripts/judge_collections.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=pc_rag_debbuging_validation
-#SBATCH --output=/home/users/caio.rhoden/slurm/%A_%a_collections_binary_classification.out
-#SBATCH --error=/home/users/caio.rhoden/slurm/%A_%a_collections_binary_classification.err
+#SBATCH --job-name=ablatation_judge_collections
+#SBATCH --output=/home/caio.rhoden/slurm/%A_%a_ablation_judge_collections.out
+#SBATCH --error=/home/caio.rhoden/slurm/%A_%a_ablation_judge_collections.err
 #SBATCH --gres=gpu:1
-#SBATCH --mem-per-gpu=20G
+#SBATCH --mem-per-gpu=40G
 #SBATCH --time=48:00:00
 #SBATCH --mail-user="c214129@dac.unicamp.br"
-#SBATCH --array=0-4
+#SBATCH --array=0-3
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --exclude=gpu03
 
 source ~/miniconda3/bin/activate
 conda activate nq
@@ -17,36 +16,37 @@ export VLLM_WORKER_MULTIPROC_METHOD=spawn
 export C_INCLUDE_PATH=$CONDA_PREFIX/include
 export CPLUS_INCLUDE_PATH=$CONDA_PREFIX/include
 
-SEEDS=(1 4 54 61 73)
-
-S_ID=$((SLURM_ARRAY_TASK_ID % 5))
-S=${SEEDS[$S_ID]}
-
+POOL_START=(550000 3050000 5600000 8050000)
+POOL_END=(2500000 5000000 7500000 10000000)
+STARTING_IDX=${POOL_START[SLURM_ARRAY_TASK_ID]}
+ENDING_IDX=${POOL_END[SLURM_ARRAY_TASK_ID]}
 echo "RUNNING SETUP"
 python run_datamodels.py \
-    --seed $S \
+    --seed 1 \
     --run_type setup
 
 echo "RUNNING COLLECTIONS TRAIN"
 python run_datamodels.py \
-    --seed $S \
+    --seed 1 \
     --run_type collections \
-    --start_idx 0 \
-    --end_idx 1000000 \
-    --checkpoint 20000 \
+    --start_idx $STARTING_IDX \
+    --end_idx $ENDING_IDX \
+    --checkpoint 50000 \
     --num_subprocesses 1 \
-    --evaluator BinaryJudge \
+    --evaluator VotingBinaryJudge \
+    --batch_size 50000 \
     --mode train
 
     
 python run_datamodels.py \
-    --seed $S \
+    --seed 1 \
     --run_type collections \
-    --start_idx 0 \
-    --end_idx 100000 \
-    --checkpoint 20000 \
+    --start_idx $STARTING_IDX \
+    --end_idx $ENDING_IDX \
+    --checkpoint 50000 \
     --num_subprocesses 1 \
-    --evaluator BinaryJudge \
+    --evaluator VotingBinaryJudge \
+    --batch_size 50000 \
     --mode test
 
     
