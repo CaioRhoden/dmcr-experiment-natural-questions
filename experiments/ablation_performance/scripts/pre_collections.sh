76803138ab869411118005ed09d842e533927646
CaioRhoden <caiorhoden@gmail.com>
2026-03-19 19:07:17 -0300

    removing problematic test set in ablation experiment

--- experiments/ablation_performance/scripts/pre_collections.sh
+++ experiments/ablation_performance/scripts/pre_collections.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=pc_ablation
-#SBATCH --output=/home/users/caio.rhoden/slurm/%A_%a_pc_ablation.out
-#SBATCH --error=/home/users/caio.rhoden/slurm/%A_%a_pc_ablation.err
+#SBATCH --output=/home/caio.rhoden/slurm/%j_pc_ablation.out
+#SBATCH --error=/home/caio.rhoden/slurm/%j_pc_ablation.err
 #SBATCH --gres=gpu:1
-#SBATCH --mem-per-gpu=139G
+#SBATCH --mem-per-gpu=20G
 #SBATCH --time=48:00:00
 #SBATCH --mail-user="c214129@dac.unicamp.br"
-#SBATCH --array=1
-#SBATCH --exclude=gpu03
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 source ~/miniconda3/bin/activate
@@ -17,34 +15,34 @@ export VLLM_WORKER_MULTIPROC_METHOD=spawn
 export C_INCLUDE_PATH=$CONDA_PREFIX/include
 export CPLUS_INCLUDE_PATH=$CONDA_PREFIX/include
 
-SEEDS=(1 4 54 61 73)
-S_ID=$((SLURM_ARRAY_TASK_ID % 5))
-S=${SEEDS[$S_ID]}
+# SEEDS=(1 4 54 61 73)
+# S_ID=$((SLURM_ARRAY_TASK_ID % 5))
+# S=${SEEDS[$S_ID]}
     
-echo "-----------------------------------------------"
-echo "RUNNING SETUP"
-python run_datamodels.py \
-    --seed $S \
-    --run_type setup
+# echo "-----------------------------------------------"
+# echo "RUNNING SETUP"
+# python run_datamodels.py \
+#     --seed $S \
+#     --run_type setup
 
-echo "-----------------------------------------------"
-echo "RUNNING PRE_COLLECTIONS TRAIN "
-python run_datamodels.py \
-    --seed $S \
-    --run_type pre_collections \
-    --start_idx 14000 \
-    --end_idx 15000 \
-    --checkpoint 1000 \
-    --mode train
-
-# echo "RUNNING PRE_COLLECTIONS TEST"
+# echo "-----------------------------------------------"
+# echo "RUNNING PRE_COLLECTIONS TRAIN "
 # python run_datamodels.py \
 #     --seed $S \
 #     --run_type pre_collections \
-#     --start_idx 0 \
-#     --end_idx 1000 \
+#     --start_idx 14000 \
+#     --end_idx 15000 \
 #     --checkpoint 1000 \
-#     --mode test
+#     --mode train
+
+echo "RUNNING PRE_COLLECTIONS TEST"
+python run_datamodels.py \
+    --seed 1 \
+    --run_type pre_collections \
+    --start_idx 0 \
+    --end_idx 1000 \
+    --checkpoint 1000 \
+    --mode test
 
 
             
