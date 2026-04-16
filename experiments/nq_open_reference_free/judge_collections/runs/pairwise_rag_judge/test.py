1443dc3e8e5636229aa5cab6516f4e8bf6868e46
CaioRhoden <caiorhoden@gmail.com>
2026-04-15 22:14:00 -0300

    missing judge collections rag pairwise

--- experiments/nq_open_reference_free/judge_collections/runs/pairwise_rag_judge/test.py
+++
@@ -1,8 +0,0 @@
-import polars as pl
-import os
-
-files = os.listdir(".")
-dfs = [pl.read_ipc(f) for f in files if f.endswith(".feather")]
-df = pl.concat(dfs)
-
-df.write_ipc("concatenated.feather")
